7a6291da6649fd0407af7981ed1564ac94598b4f
Outvi V <git@outv.im>
2025-11-27 23:49:12 +0800

    chore: remove qaengblog
    QA Eng blog has native RSS

--- feed/index.ts
+++ feed/index.ts
@@ -3,9 +3,8 @@ import sample from './sample.ts'
 import idolypride from './idolypride.ts'
 import sp_denonbu from './sp_denonbu.ts'
 import sfti from './sfti.ts'
-import qaengblog from './qaengblog.ts'
 
-const _: Feeder[] = [sample, idolypride, sp_denonbu, sfti, qaengblog]
+const _: Feeder[] = [sample, idolypride, sp_denonbu, sfti]
 
 export const Proxies = {
   '/cloudflare': 'https://blog.cloudflare.com/rss/',
